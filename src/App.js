bc0876bfec41ba6cbc3b94e8e5af5928c05b4014
C-S-Darshan <darshan.cs1113@gmail.com>
2024-01-31 20:04:02 +0530

    Added Authentication function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Authenticate from './Components/Authentication';
+import AccountProvider from './Context/AccountProvider';
 
 function App() {
   return (
     <div className="App">
+      <AccountProvider>
       <h1>Chat app based on FourQ ECC</h1>
       <Authenticate/>
+      </AccountProvider>
     </div>
   );
 }
--- src/Components/Authentication.jsx
+++ src/Components/Authentication.jsx
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useState,useContext } from "react";
+import { AccountContext } from "../Context/AccountProvider";
 
 function Authenticate(){
+    const [clientName,setClientName] = useState('')
+    const [clientPassword,setPassword] = useState('')
+    const {setAccount} = useContext(AccountContext)
 
-
+    const authenticateUser = async () => {
+        try{
+            const response = await fetch('http://localhost:8080/api/sendData',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({clientName,clientPassword})
+        })
+        if(!response.ok){
+            throw new Error('Network respose was not ok')
+        }
+        const data = await response.json()
+        console.log('Data sent successfully')
+        setAccount(data)
+        }
+        catch{
+            console.log('Something happened to fetch')
+        }
+    }
     return(
         <>
             <p>Please provide your details for Authentication</p>
             <p>Enter your name: </p>
-            <input type="text" name="ClientName" id="CName"/><br/>
+            <input type="text" name="ClientName" id="CName" placeholder="Name" onChange={(e)=>setClientName(e.target.value)}/><br/>
             <p>Enter your Password: </p>
-            <input type="text" name="ClientPassword" id="CPassWord"/><br/>
-            <button >Submit</button>
+            <input type="text" name="ClientPassword" id="CPassWord" placeholder="Password" onChange={(e)=>setPassword(e.target.value)}/><br/>
+            <button onClick={authenticateUser}>Submit</button>
         </>
     )
 }
